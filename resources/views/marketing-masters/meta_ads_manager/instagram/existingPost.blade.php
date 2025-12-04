f88d1ca3265999d75a9c9475f8700547457482c5
Rashmi Bhavsar <software2@5core.com>
2025-12-04 21:23:36 +0530

    Meta Ads Views Splited Based on Ad Type and Facebook/Instagram

---
+++ resources/views/marketing-masters/meta_ads_manager/instagram/existingPost.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.vertical', ['title' => 'Meta - Instagram Existing Post', 'mode' => $mode ?? '', 'demo' => $demo ?? ''])
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+    @include('marketing-masters.meta_ads_manager.partials.styles')
+@endsection
+@section('content')
+    @include('layouts.shared.page-title', [
+        'page_title' => 'Instagram Existing Post Ads',
+        'sub_title' => 'Instagram Existing Post',
+    ])
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow-sm">
+                <div class="card-body py-3">
+                    @include('marketing-masters.meta_ads_manager.partials.filters', [
+                        'adType' => 'INSTAGRAM EXISTING POST',
+                        'latestUpdatedAt' => $latestUpdatedAt ?? null
+                    ])
+                    <div id="budget-under-table"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://unpkg.com/tabulator-tables@6.3.1/dist/js/tabulator.min.js"></script>
+    <script>
+        var dataUrl = "{{ route('meta.ads.instagram.existing.post.data') }}";
+        var adType = "Instagram Existing Post";
+    </script>
+    @include('marketing-masters.meta_ads_manager.partials.table-script-full')
+@endsection
--- resources/views/marketing-masters/meta_ads_manager/instagram/singleVideo.blade.php
+++ resources/views/marketing-masters/meta_ads_manager/instagram/singleVideo.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.vertical', ['title' => 'Meta - Carousal', 'mode' => $mode ?? '', 'demo' => $demo ?? ''])
+@extends('layouts.vertical', ['title' => 'Meta - Instagram Single Video', 'mode' => $mode ?? '', 'demo' => $demo ?? ''])
 @section('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://unpkg.com/tabulator-tables@6.3.1/dist/css/tabulator.min.css" rel="stylesheet">
@@ -7,15 +7,15 @@
 @endsection
 @section('content')
     @include('layouts.shared.page-title', [
-        'page_title' => 'Carousal Ads',
-        'sub_title' => 'Carousal',
+        'page_title' => 'Instagram Single Video Ads',
+        'sub_title' => 'Instagram Single Video',
     ])
     <div class="row">
         <div class="col-12">
             <div class="card shadow-sm">
                 <div class="card-body py-3">
                     @include('marketing-masters.meta_ads_manager.partials.filters', [
-                        'adType' => 'CAROUSAL',
+                        'adType' => 'INSTAGRAM SINGLE VIDEO',
                         'latestUpdatedAt' => $latestUpdatedAt ?? null
                     ])
                     <div id="budget-under-table"></div>
@@ -29,8 +29,8 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://unpkg.com/tabulator-tables@6.3.1/dist/js/tabulator.min.js"></script>
     <script>
-        var dataUrl = "{{ route('meta.ads.carousal.data') }}";
-        var adType = "Carousal";
+        var dataUrl = "{{ route('meta.ads.instagram.single.video.data') }}";
+        var adType = "Instagram Single Video";
     </script>
     @include('marketing-masters.meta_ads_manager.partials.table-script-full')
 @endsection
