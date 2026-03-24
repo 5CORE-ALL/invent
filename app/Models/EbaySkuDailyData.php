4875496b96ff8f70879450fbdfbaf195bb830a23
Aditya <aadityasrivastava99@gmail.com>
2026-03-25 00:23:05 +0530

    Updated code
    Updated code

--- app/Models/EbaySkuDailyData.php
+++ app/Models/EbaySkuDailyData.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Daily snapshot of eBay listing metrics per SKU (see {@see \App\Console\Commands\CollectEbayMetrics}).
+ *
+ * {@see $daily_data} typically includes cumulative `views` from the metrics API; PMT Ads derives
+ * period views via day-over-day deltas and cumulative window math in {@see \App\Http\Controllers\Campaigns\EbayPMPAdsController::computeEbayDailyViewAggregates}.
+ */
 class EbaySkuDailyData extends Model
 {
     use HasFactory;
